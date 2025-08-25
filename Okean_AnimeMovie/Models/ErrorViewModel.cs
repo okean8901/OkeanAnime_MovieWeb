f45d943058b07d5e887f0d7c93e27073cc24593f
okean8901 <trangialoc892001@gmail.com>
2025-08-25 12:58:28 +0700

    first commit

---
+++ Okean_AnimeMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Okean_AnimeMovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
